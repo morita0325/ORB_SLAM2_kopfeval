4fee2ce0f43497614002e5841f906fbce4baca6b
morita0325 <morita@silexica.com>
2021-01-21 10:00:41 +0900

    add popd to arrange directory

--- build.sh
+++ build.sh
@@ -3,23 +3,25 @@ git submodule update init
 #EIGEN
 git clone https://gitlab.com/libeigen/eigen.git
 pushd eigen
-mkdir build
+mkdir -p build
 pushd build
 cmake  ../ -DCMAKE_INSTALL_PREFIX=../../eigen3
 make install
+popd
 popd
 
 #OPENCV
 git clone -b 3.4 https://github.com/opencv/opencv.git
 git clone -b 3.4 https://github.com/opencv/opencv_contrib.git
 pushd opencv
-mkdir build
+mkdir -p build
 cmake -DOPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules ../
 make -j4
 popd
 
 
 #ORB_SLAM2
+git clone https://github.com/raulmur/ORB_SLAM2.git ORB_SLAM2
 pushd ORB_SLAM2
 chmod +x build.sh
 ./build.sh
@@ -28,6 +30,6 @@ popd
 #dataset
 mkdir dataset
 pushd dataset
-wget https://vision.in.tum.de/rgbd/dataset/freiburg1/rgbd_dataset_freiburg1_rpy.tgz
+wget -nc https://vision.in.tum.de/rgbd/dataset/freiburg1/rgbd_dataset_freiburg1_rpy.tgz
 tar xvf rgbd_dataset_freiburg1_rpy.tgz
 popd
